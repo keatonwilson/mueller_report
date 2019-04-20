e2b9672d93a06616aa09a7e2753e04aaddde8101
keatonwilson <keatonwilson@me.com>
2019-04-20 16:33:32 -0700

    Pushed script.

---
+++ corpus_worldcloud.R
@@ -0,0 +1,24 @@
+#Mueller Report Wordcloud
+#Keaton Wilson
+#keatonwilson@me.com
+#2019-04-19
+
+#libraries
+library(readtext)
+library(tidyverse)
+library(SnowballC)
+library(wordcloud)
+library(tm)
+
+#Reading in the text file
+raw = readLines("./mueller_text.txt", encoding = "UTF-8")
+
+data = VCorpus(VectorSource(raw), readerControl=list(readPlain, language="en", load=TRUE))
+
+corp = tm_map(data, removePunctuation)
+corp = tm_map(corp, stripWhitespace)
+corp = tm_map(corp, content_transformer(tolower))
+corp = tm_map(corp, function(x)removeWords(x,stopwords()))
+corp = tm_map(corp, function(x)removeNumbers(x))
+
+wc = wordcloud(corp, scale = c(4.5, 1), min.freq = 15, colors = "black", max.words = 100)
